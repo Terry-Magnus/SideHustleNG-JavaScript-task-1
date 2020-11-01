bda42c6734e1b26baecbf193842c1483c8198307
Terry-Magnus <66229464+Terry-Magnus@users.noreply.github.com>
2020-11-01 12:41:48 +0100

    Add files via upload

---
+++ EarningCalculator.js
@@ -0,0 +1,16 @@
+// Salary Calculator 
+/* A simple program to calculate daily earnings
+*/
+function salaryCalc(hrs) {
+	var wage = 4000; // wage is already fixed since 1000/hr and normal time is 4hrs
+	if(hrs <= 4) { 
+		var tip = 0;	//no overtime earnings if hrs <= 4
+	}
+	else {
+		var exthr = hrs - 4; //number of hrs inputed is used to calculate overtime
+		var tip = (200 * exthr); // overtime is at a rate of 200/hr
+	}
+	
+	var earnings = (wage + tip); //total earnings for the day
+	return "Employee earnings = " + earnings + " naira."
+}
